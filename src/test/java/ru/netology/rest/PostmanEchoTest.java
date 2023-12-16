23970a73872df4454a1e126e1f8d1d3d4c477838
ArtVysh <vysh-artem@yandex.ru>
2023-12-16 16:26:42 +0400

    Crash test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -21,7 +21,7 @@ public class PostmanEchoTest {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Элементарно, Ватсон!"))
+                .body("data", equalTo("Овсянка, сэр!"))
         ;
     }
 }
